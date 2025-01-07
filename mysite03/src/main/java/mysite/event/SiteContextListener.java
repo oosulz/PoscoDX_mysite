9067e811b560ec580cb10d2d60cb2b66f9d74d63
bakukun <h1101201@gmail.com>
2025-01-07 09:07:56 +0900

    ServletContext's Attribute Map

---
+++ mysite03/src/main/java/mysite/event/SiteContextListener.java
@@ -0,0 +1,26 @@
+package mysite.event;
+
+import org.springframework.context.ApplicationContext;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import mysite.service.SiteService;
+import mysite.vo.SiteVo;
+
+public class SiteContextListener implements ServletContextListener {
+	
+    private SiteService siteService;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        ApplicationContext springContext = 
+            (ApplicationContext) context.getAttribute("org.springframework.web.context.WebApplicationContext.ROOT");
+        
+        this.siteService = springContext.getBean(SiteService.class);
+        SiteVo siteVo = siteService.getSite();
+        
+        context.setAttribute("siteVo", siteVo);
+    }
+}          
--- mysite03/src/main/java/mysite/interceptor/SiteInterceptor.java
+++ mysite03/src/main/java/mysite/interceptor/SiteInterceptor.java
@@ -1,32 +1,26 @@
 package mysite.interceptor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.LocaleResolver;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import mysite.service.SiteService;
-import mysite.vo.SiteVo;
 
 public class SiteInterceptor implements HandlerInterceptor {
 	private LocaleResolver localeResolver;
-	private SiteService siteService;
+
 
 	public SiteInterceptor(LocaleResolver localeResolver,SiteService siteService) {
 		this.localeResolver = localeResolver;
-		this.siteService = siteService;
+
 	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		// locale
 		String lang = localeResolver.resolveLocale(request).getLanguage();
-		SiteVo siteVo = siteService.getSite();
 		request.setAttribute("lang", lang);
-		request.setAttribute("siteVo", siteVo);
 		return true; 
 	}
-
+	
 }
